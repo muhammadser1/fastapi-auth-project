f0e33ed9854a19d97c25fcb95fc9df983a39febe
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-07-10 03:12:16 +0300

    Implemented a login endpoint in FastAPI for user authentication.
    This endpoint validates the user's credentials against stored hashed passwords in the database. It checks if the provided username exists and verifies the password using bcrypt for security. Upon successful authentication, the endpoint returns user details. In case of invalid credentials, appropriate HTTP 401 Unauthorized errors are raised to maintain security.

--- user_auth_service/controllers/user_controller.py
+++ user_auth_service/controllers/user_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from models.schemas import User
 from models.Mysql_database import get_db
 from utils.password_utils import hash_password
+from utils.password_utils import verify_password
 
 user_router = APIRouter()
 
@@ -35,8 +36,28 @@ def sign_up(user: User, auth_db=Depends(get_db)):
 
         # Add the user to the database
         auth_db.add_user_to_db(user)
+        # token
 
         return {"message": "User signed up successfully"}
 
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Failed to process the request") from e
+
+@user_router.post("/token")
+def login_in(username: str, password: str, auth_db=Depends(get_db)):
+    """
+    Endpoint to authenticate a user
+
+    :param username: Username for authentication.
+    :param password: Password for authentication.
+    :param auth_db: Dependency to access the database.
+    :return: Message indicating successful authentication
+    """
+    existing_user = auth_db.get_user_by_username(username)
+
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    if not verify_password(existing_user["password_hash"], password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    #token
+    return (existing_user)
